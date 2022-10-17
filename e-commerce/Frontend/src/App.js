a0b69219cba1ea28679592b98894f4faeab4b5fb
Ehab-darawsheh <ehabdarawsheh07@gmail.com>
2022-10-18 02:48:58 +0300

    Create CartScreen component

--- e-commerce/Frontend/src/App.js
+++ e-commerce/Frontend/src/App.js
@@ -9,6 +9,7 @@ import Container from "react-bootstrap/Container";
 import { LinkContainer } from "react-router-bootstrap";
 import { Link } from "react-router-dom";
 import { Store } from "./store";
+import CartScreen from "./screens/CartScreen";
 
 function App() {
   const { state } = useContext(Store);
@@ -42,6 +43,7 @@ function App() {
           <Container className="mt-3">
             <Routes>
               <Route path="/product/:slug" element={<ProductScreen />} />
+              <Route path="/cart" element={<CartScreen />} />
               <Route path="/" element={<HomeScreen />} />
             </Routes>
           </Container>
