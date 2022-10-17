4a5fa8c903238bb9fb0e10d7290c8e297ee4c7e1
Ehab-darawsheh <ehabdarawsheh07@gmail.com>
2022-10-17 19:00:15 +0300

    Add Product Screen

--- e-commerce/Frontend/src/screens/HomeScreen.js
+++ e-commerce/Frontend/src/screens/HomeScreen.js
@@ -5,6 +5,7 @@ import axios from "axios";
 import logger from "use-reducer-logger";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import {Helmet} from 'react-helmet-async'
 
 
 const reducer = (state, action) => {
@@ -42,6 +43,9 @@ const HomeScreen = () => {
   }, []);
   return (
     <div>
+      <Helmet>
+        <title>E-commerce</title>
+      </Helmet>
       <h1>Featured Products</h1>
       <div className="products">
         {loading ? (
