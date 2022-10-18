e76a0e8cef975aa382e46fd44a1ab2e5a3208e1c
Ehab-darawsheh <ehabdarawsheh07@gmail.com>
2022-10-18 20:18:24 +0300

    create signin backend api

--- e-commerce/Backend/server.js
+++ e-commerce/Backend/server.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const seedRouter = require("./routes/seedRoutes.js");
 const productRouter = require("./routes/productRoutes");
+const userRouter = require("./routes/userRoutes.js");
 
 dotenv.config();
 
@@ -18,13 +19,20 @@ mongoose
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+
+
 app.use("/api/seed", seedRouter);
 app.use("/api/products", productRouter);
+app.use("/api/users", userRouter);
 
-
+app.use((err, req, res, next) => {
+  res.status(500).send({message: err.message});
+});
 
 const port = 5000;
-
 app.listen(port, () => {
   console.log("Runing");
 });
