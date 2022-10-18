394ba57dc0370e190bcfc1a9aa3d1020f84e451d
Ehab-darawsheh <ehabdarawsheh07@gmail.com>
2022-10-18 16:19:03 +0300

    Connect to MongoDB

--- e-commerce/Backend/server.js
+++ e-commerce/Backend/server.js
@@ -1,32 +1,43 @@
 const express = require("express");
-const  data  = require("./data.js");
+const data = require("./data.js");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log("connected");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 
 const app = express();
 
 app.get("/api/products", (req, res) => {
-    res.send(data.products);
+  res.send(data.products);
 });
 app.get("/api/products/slug/:slug", (req, res) => {
-    const product = data.products.find(x => x.slug === req.params.slug)
-    if(product)
-    {
-        res.send(product)
-    }else{
-        res.status(404).send({message:'Product not found' })
-    }
+  const product = data.products.find((x) => x.slug === req.params.slug);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: "Product not found" });
+  }
 });
 app.get("/api/products/:id", (req, res) => {
-    const product = data.products.find(x => x._id === req.params.id)
-    if(product)
-    {
-        res.send(product)
-    }else{
-        res.status(404).send({message:'Product not found' })
-    }
+  const product = data.products.find((x) => x._id === req.params.id);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: "Product not found" });
+  }
 });
 
 const port = 5000;
 
-app.listen(port,() =>{
-    console.log('Runing');
-} )
+app.listen(port, () => {
+  console.log("Runing");
+});
