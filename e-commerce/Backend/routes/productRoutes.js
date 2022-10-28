1371468a0dee3191e20809e9a2c893502a61083b
Ehab-darawsheh <ehabdarawsheh07@gmail.com>
2022-10-28 03:16:27 +0300

    Add Side Bar and Search Box

--- e-commerce/Backend/routes/productRoutes.js
+++ e-commerce/Backend/routes/productRoutes.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const expressAsyncHandler = require("express-async-handler");
 const Product = require("../models/productModel.js");
 
 const productRouter = express.Router();
@@ -8,15 +9,24 @@ productRouter.get("/", async (req, res) => {
   res.send(products);
 });
 
-productRouter.get("/slug/:slug", async(req, res) => {
-  const product = await Product.findOne({slug:req.params.slug});
+productRouter.get(
+  '/categories',
+  expressAsyncHandler(async (req, res) => {
+    const categories = await Product.find().distinct('category');
+    res.send(categories);
+  })
+);
+
+
+productRouter.get("/slug/:slug", async (req, res) => {
+  const product = await Product.findOne({ slug: req.params.slug });
   if (product) {
     res.send(product);
   } else {
     res.status(404).send({ message: "Product not found" });
   }
 });
-productRouter.get("/:id", async(req, res) => {
+productRouter.get("/:id", async (req, res) => {
   const product = await Product.findById(req.params.id);
   if (product) {
     res.send(product);
@@ -24,4 +34,5 @@ productRouter.get("/:id", async(req, res) => {
     res.status(404).send({ message: "Product not found" });
   }
 });
+
 module.exports = productRouter;
--- e-commerce/Frontend/src/App.js
+++ e-commerce/Frontend/src/App.js
@@ -1,5 +1,5 @@
-import React, { useContext } from "react";
-import { ToastContainer } from "react-toastify";
+import React, { useContext, useEffect, useState } from "react";
+import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomeScreen from "./screens/HomeScreen";
@@ -20,6 +20,10 @@ import PaymentMethodScreen from "./screens/PaymentMethodScreen";
 import PlaceOrderScreen from "./screens/PlaceOrderScreen";
 import OrderScreen from "./screens/OrderScreen";
 import ProfileScreen from "./screens/ProfileScreen";
+import Button from "react-bootstrap/Button";
+import { getError } from "./utils";
+import axios from "axios";
+import SearchBox from "./components/SearchBox";
 
 function App() {
   const { state, dispatch: ctxDispatch } = useContext(Store);
@@ -32,18 +36,47 @@ function App() {
     localStorage.removeItem("shippingAddress");
     localStorage.removeItem("paymentMethod");
   };
+  const [sidebarIsOpen, setSidebarIsOpen] = useState(false);
+  const [categories, setCategories] = useState([]);
 
+  useEffect(() => {
+    const fetchCategories = async () => {
+      try {
+        const { data } = await axios.get(`/api/products/categories`);
+        setCategories(data);
+      } catch (err) {
+        toast.error(getError(err));
+      }
+    };
+    fetchCategories();
+  }, []);
   return (
     <BrowserRouter>
-      <div className="d-flex flex-column site-container">
+      <div
+        className={
+          (sidebarIsOpen)
+             ?"d-flex flex-column site-container active-cont"
+            : "d-flex flex-column site-container"
+        }
+      >
         <ToastContainer position="bottom-center" limit={1} />
         <header>
           <Navbar bg="dark" variant="dark">
             <Container>
+              <Button
+                variant="dark"
+                onClick={() => setSidebarIsOpen(!sidebarIsOpen)}
+              >
+                <i className="fas fa-bars"></i>
+              </Button>
+
               <LinkContainer to="/">
                 <Navbar.Brand>E-comemrce</Navbar.Brand>
               </LinkContainer>
-              <Nav>
+              <Navbar.Toggle aria-controls="basic-navbar-nav" />
+              <Navbar.Collapse id="basic-navbar-nav">
+              <SearchBox />
+              <Nav className="me-auto  w-100  justify-content-end">
                 <Link to="/cart" className="nav-link">
                   Cart
                   {cart.cartItems.length > 0 && (
@@ -72,9 +105,33 @@ function App() {
                   </Link>
                 )}
               </Nav>
+              </Navbar.Collapse>
             </Container>
           </Navbar>
         </header>
+        <div
+          className={
+            (sidebarIsOpen)
+              ? 'active-nav side-navbar d-flex justify-content-between flex-wrap flex-column'
+              : 'side-navbar d-flex justify-content-between flex-wrap flex-column'
+          }
+        >
+          <Nav className="flex-column text-white w-100 p-2">
+            <Nav.Item>
+              <strong>Categories</strong>
+            </Nav.Item>
+            {categories.map((category) => (
+              <Nav.Item key={category}>
+                <LinkContainer
+                  to={`/search category=${category}`}
+                  onClick={() => setSidebarIsOpen(false)}
+                >
+                  <Nav.Link>{category}</Nav.Link>
+                </LinkContainer>
+              </Nav.Item>
+            ))}
+          </Nav>
+        </div>
         <main>
           <Container className="mt-3">
             <Routes>
